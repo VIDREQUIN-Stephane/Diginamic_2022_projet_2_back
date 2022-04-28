620f44e3d6db98141be2d36e60273c91358d9076
Magydream <87599508+Magydream@users.noreply.github.com>
2022-04-28 12:21:23 +0200

    ajout dossier css js assets + personnalisation du css

--- templates/head.php
+++ templates/head.php
@@ -1,10 +1,18 @@
+<html lang="fr" country="FR">
 
-<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=6.0">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index,follow">
+    <meta name="description" content="La société qui conçoit et commercialise des panneaux solaires à haut
+    rendement (25%). Dans le cadre de ses activités Sunpower a développé des
+    ombrières solaires pour la recharge des véhicules électriques">
+    <meta name="theme-color" content="">
+    <meta name="msapplication-TileColor" content="">
+    <link rel="shortcut icon" type="image/png" href="/assets/images/favicon.ico" />
+    <link rel="stylesheet" href="../assets/css/main.css">
     <title><?= $_GET['page'] ?? "Mon site" ?></title>
+
 </head>
