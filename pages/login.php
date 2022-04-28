ac9eb1b3f060d6aa129b969d04d40dc499842b66
Isaacaal <94602304+Isaacaal@users.noreply.github.com>
2022-04-28 16:41:17 +0200

    Add files via upload

---
+++ pages/login.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+include 'config.php';
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=trekker', $user, $pass);
+}
+catch (exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+if (isset($_SESSION['id'])) {
+    $getid = intval($_SESSION['id']);
+	$requser = $bdd->prepare('SELECT * FROM utilisateur WHERE id = ?');
+	$requser->execute(array($getid));
+	$userinfo = $requser->fetch();
+    ?>
+    <h2>Profil de <?php echo $userinfo['prenom']; ?> <?php echo $userinfo['nom']; ?></h2>
+    Adresse mail : <?php echo $userinfo['email']; ?>
+    <?php
+    if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id'])
+    {
+    ?>
+    <a href="edit.php">Modifier mon profil</a> <br>
+    <a href="unlog.php">Se déconnecter</a><br><br><br>
+    <?php
+    }
+} else {
+    //******************************** */
+    //****** Cookies à inclure *********/
+    //******************************** */
+    // include_once('cookie.php');
+    if(isset($_POST['emailConnect']))
+    {
+        $emailConnect = htmlspecialchars($_POST['emailConnect']);
+        $mdpConnect = sha1($_POST['mdpConnect']);
+        if(!empty($emailConnect) AND !empty($mdpConnect)) 
+        {
+            $requser = $bdd->prepare('SELECT * FROM utilisateur WHERE email = ? AND password = ?');
+            $requser->execute(array($emailConnect, $mdpConnect));
+            $userexist = $requser->rowCount();
+            if($userexist == 1)
+            {
+                if(isset($_POST['rememberMe']))
+                {
+                    setcookie('email', $emailConnect, time()+365*24*3600,null,null,false,true);
+                    setcookie('password', $mdpConnect, time()+365*24*3600,null,null,false,true);
+                }
+                $userinfo = $requser->fetch();
+                $_SESSION['id'] = $userinfo['id'];
+                $_SESSION['email'] = $userinfo['email'];
+                $id = $_SESSION['id'];
+                header("Location: profil.php");
+            }
+            else
+            {
+                $error = 'Adresse mail ou mot de passe invalide.';
+            }
+        }
+        else
+        {
+            $error = 'Veuillez remplir tous les champs.';
+        }
+    }
+    ?>
+    <!-- FORMULAIRE CONNECTION -->
+    <form action='login.php' method="post">
+        <!-- ADRESSE MAIL -->
+        <label for="emailConnect">
+            Adresse mail : 
+            <input type="email" name="emailConnect" id="emailConnect">
+        </label><br>
+        <!-- MOT DE PASSE -->
+        <label for="mdpConnect">
+            Mot de passe : 
+            <input type="password" name="mdpConnect" id="mdpConnect">
+        </label><br>
+        <!-- REMEMBER ME -->
+        <label for="rememberMe">
+        Se souvenir de moi
+            <input type="checkbox" name="rememberMe" id="rememberMe">
+        </label><br>
+        <!-- SUBMIT -->
+        <input type="submit" value="Se connecter">
+    </form>
+    <?php
+        if (isset($error)) {
+            echo '<font color="red" class="error">' . $error . '</font>';
+        }
+}
+?>
---
+++ pages/register.php
@@ -0,0 +1,91 @@
+<?php
+include_once "config.php";
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=trekker', $user, $pass);
+}
+catch (exception $e) {
+	die('Erreur : ' . $e->getMessage());
+}
+// VALIDATION DU SUBMIT
+if (isset ($_POST['submit'])) {
+    // ASSIGNATION DES VARIABLES
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $nom = htmlspecialchars($_POST['nom']);
+    $email = htmlspecialchars($_POST['email']);
+    $mdp = sha1($_POST['mdp']);
+    $mdp_ = sha1($_POST['mdp_']);
+    // FORMULAIRE NON VIDE
+    if(!empty($_POST['prenom'])
+        AND !empty ($_POST['nom'])
+        AND !empty ($_POST['email'])
+        AND !empty ($_POST['mdp'])
+        AND !empty ($_POST['mdp_'])) {
+            // EMAIL AU FORMAT VALIDE
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // MAIL NON ULTILISE
+            $reqmail = $bdd->prepare('SELECT * FROM utilisateur WHERE email = ?');
+            $reqmail->execute(array($email));
+            $mailexist = $reqmail->rowCount();
+            if ($mailexist == 0) {
+                // PASSWORD ET CONFIRMATION IDENTIQUES
+                if(($_POST['mdp']) == ($_POST['mdp_'])) {
+                    // INSERTION DANS LA BASE DE DONNEE
+                    $insertmbr = $bdd->prepare('INSERT INTO utilisateur(nom, prenom, email, password) VALUES(?, ?, ?, ?)');
+                    $insertmbr->execute(array($nom, $prenom, $email, $mdp));
+                    $error = 'Compte créé. <a href="login.php">Se connecter</a>';
+                } else {
+                    $error = 'Les mots de passe ne sont pas identiques, veuillez réessayer.';
+                }
+            } else {
+                $error = 'L\'adresse email est déjà utilisée.';
+            }
+        } else {
+            $error = 'L\'adresse mail utilisée n\'est pas valide.';
+        }
+    } else {
+        $error = 'Veuillez remplir tous les champs.';
+    }
+}
+?>
+<!-- FORMAULAIRE D'ENREGISTREMENT -->
+<form action="" method="post">
+    <!-- NOM -->
+    <label for="nom">
+        Nom : 
+        <input type="text" name="nom" id="nom" value="<?php if(isset($nom)) { echo $nom; } ?>">
+    </label> <br>
+    <!-- PRENOM -->
+    <label for="prenom">
+        Prenom : 
+        <input type="text" name="prenom" id="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+    </label> <br>
+    <!-- EMAIL -->
+    <label for="email">
+        E-mail : 
+        <input type="email" name="email" id="email" value="<?php if(isset($email)) { echo $email; } ?>">
+    </label> <br>
+    <!-- MOT DE PASSE -->
+    <label for="mdp">
+        Mot de passe : 
+        <input type="password" name="mdp" id="mdp">
+    </label> <br>
+    <!-- CONFIRMATION MOT DE PASSE -->
+    <label for="mdp_">
+        Confirmation mot de passe : 
+        <input type="password" name="mdp_" id="mdp_">
+    </label> <br>
+    <!-- PHOTO -->
+    <label for="photo">
+        Photo de profil : 
+        <input type="file" name="photo" id="photo">
+    </label> <br>
+    <!-- SUBMIT -->
+    <input type="submit" value="Je m'enregistre" name="submit" id="sumbit">
+</form>
+<a href="login.php">déjà inscrit ?</a><br>
+<?php
+    if (isset($error))
+    {
+        echo '<font color="red">' . $error . "</font>";
+    }
+?>
