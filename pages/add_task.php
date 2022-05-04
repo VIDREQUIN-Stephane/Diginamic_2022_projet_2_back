6918940afa15b704669e726c308d971e7c6cf3f5
VIDREQUIN <77000533+VIDREQUIN-Stephane@users.noreply.github.com>
2022-05-04 13:02:21 +0200

    Update add_task.php

--- pages/add_task.php
+++ pages/add_task.php
@@ -3,8 +3,8 @@
 try{
    if (is_connected()){ // vérifie la connection au serveur
       try{
-         if (isset($_POST['nomTache']) AND $_POST['nomTache']!=NULL){ // +++ créer le formulaire de création de tache dans form_add_task +++
-         $newTask= new Tache($_POST['nom'], $_POST['depart'], $_POST['echeance'], $_POST['comment']); // je crée la tache en vu de son enregistrement en base
+         if (isset($_POST['nomTask']) AND $_POST['nomTask']!=NULL){ // +++ créer le formulaire de création de tache dans form_add_task +++
+         $newTask= new Tache($_POST['nomTask'], $_POST['dateStart'], $_POST['echeance'], $_POST['comment']); // je crée la tache en vu de son enregistrement en base
          } else {
             echo 'vous devez donner un nom à votre tâche';
          }
